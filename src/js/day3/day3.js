5890a52f344ce1abd557170f0370e89e746acbc7
Kasia <katarzyna-godlewska1@wp.pl>
2020-01-18 23:26:42 +0100

    Day 3

---
+++ src/js/day3/day3.js
@@ -0,0 +1,57 @@
+
+//ex 1
+/*let base = prompt('Enter a base of the triangle');
+let height = prompt('Enter a height of the triangle');
+let area = base * height * 0.5
+alert(`The area is ${area}`);
+*/
+//ex 2
+/*let sideA = prompt('Enter a side a of triangle');
+let sideB = prompt('Enter a side b of triangle');
+let sideC = prompt('Enter a side c of triangle');
+let perimeter = parseInt(sideA) + parseInt(sideB) + parseInt(sideC);
+console.log(`Enter side a: ${sideA}`);
+console.log(`Enter side b: ${sideB}`);
+console.log(`Enter side c: ${sideC}`);
+console.log(`The perimeter of the triangle is ${perimeter}`);
+*/
+
+//ex 3
+/*let length = prompt('Enter a length of rectangle');
+let width = prompt('Enter a width of rectangle');
+let rectangleArea = length * width;
+let rectanglePerimeter = 2 * (parseInt(length) + parseInt(width));
+console.log(`Enter length: ${length}`);
+console.log(`Enter length: ${width}`);
+console.log(`The perimeter o the rectangle is ${rectanglePerimeter} and the area is ${rectangleArea}`);
+*/
+
+//ex 4
+/*let PI = 3.14;
+let radius = prompt('Enter radius of the circle');
+let circleArea = PI * radius * radius;
+let circleCircumference = 2 * PI * radius;
+console.log(`Enter radius: ${radius}`);
+console.log(`The area of the circle is ${circleArea} and the circumerence is ${circleCircumference}`);
+*/
+
+//ex 5
+let a = 2;
+let b = -2;
+let y0 = a * 0 + b;
+let x0 = 2 / 2;
+console.log(`y0 = ${y0}`);
+console.log(`x0 = ${x0}`);
+
+//ex 9
+let hours = prompt('Enter hours');
+let ratePerHour = prompt('Enter rate per hour');
+let earning = hours * ratePerHour;
+console.log(`Your weekly earning is ${earning}`);
+
+//ex 10
+let myName = 'Katarzyna';
+myName.length > 7
+    ? console.log('My name is long')
+    : console.log('My name is short');
+
