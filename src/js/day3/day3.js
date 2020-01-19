e3b07549c12ea55e1d0eab82ff8ec8211af1ee24
Kasia <katarzyna-godlewska1@wp.pl>
2020-01-19 19:18:22 +0100

    uncomment prompt exercises

--- src/js/day3/day3.js
+++ src/js/day3/day3.js
@@ -1,12 +1,12 @@
 
 //ex 1
-/*let base = prompt('Enter a base of the triangle');
+let base = prompt('Enter a base of the triangle');
 let height = prompt('Enter a height of the triangle');
 let area = base * height * 0.5
 alert(`The area is ${area}`);
-*/
+
 //ex 2
-/*let sideA = prompt('Enter a side a of triangle');
+let sideA = prompt('Enter a side a of triangle');
 let sideB = prompt('Enter a side b of triangle');
 let sideC = prompt('Enter a side c of triangle');
 let perimeter = parseInt(sideA) + parseInt(sideB) + parseInt(sideC);
@@ -14,26 +14,23 @@ console.log(`Enter side a: ${sideA}`);
 console.log(`Enter side b: ${sideB}`);
 console.log(`Enter side c: ${sideC}`);
 console.log(`The perimeter of the triangle is ${perimeter}`);
-*/
 
 //ex 3
-/*let length = prompt('Enter a length of rectangle');
+let length = prompt('Enter a length of rectangle');
 let width = prompt('Enter a width of rectangle');
 let rectangleArea = length * width;
 let rectanglePerimeter = 2 * (parseInt(length) + parseInt(width));
 console.log(`Enter length: ${length}`);
 console.log(`Enter length: ${width}`);
 console.log(`The perimeter o the rectangle is ${rectanglePerimeter} and the area is ${rectangleArea}`);
-*/
 
 //ex 4
-/*let PI = 3.14;
+let PI = 3.14;
 let radius = prompt('Enter radius of the circle');
 let circleArea = PI * radius * radius;
 let circleCircumference = 2 * PI * radius;
 console.log(`Enter radius: ${radius}`);
 console.log(`The area of the circle is ${circleArea} and the circumerence is ${circleCircumference}`);
-*/
 
 //ex 5
 let a = 2;
