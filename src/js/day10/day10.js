eae261421c749064c82ca8003a2d68795f909589
Kasia <katarzyna-godlewska1@wp.pl>
2020-02-05 23:46:34 +0100

    Day 10

---
+++ src/js/day10/day10.js
@@ -0,0 +1,23 @@
+const a = [4, 5, 8, 9]
+const b = [3, 4, 5, 7]
+const countries = ['Finland', 'Sweden', 'Norway']
+
+let c = [...a, ...b]
+
+let set = new Set()
+console.log(set);
+
+let A = new Set(a)
+let B = new Set(b)
+let C = new Set(c)
+
+console.log(C);
+
+let d = a.filter(num => B.has(num))
+let D = new Set (d);
+console.log(D);
+
+setOfCountries = new Set (countries)
+setOfCountries.delete('Sweden')
+console.log(setOfCountries.size)
+
