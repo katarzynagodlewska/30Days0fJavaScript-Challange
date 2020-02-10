44363f161c7b94198d5eab2195c85e528e1a30bc
Kasia <katarzyna-godlewska1@wp.pl>
2020-02-10 23:47:52 +0100

    Day 12

---
+++ src/js/day12/day12.js
@@ -0,0 +1,4 @@
+const text = 'He earns 4000 euro from salary per month, 10000 euro annual bonus, 5500 euro online courses per month.'
+const pattern = /\d+/g
+const matches = text. match(pattern)
+console.log(matches)
