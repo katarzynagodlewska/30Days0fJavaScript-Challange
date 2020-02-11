17fdee10a85e0ee28d7609c1bae92b3c8b72c51a
Kasia <katarzyna-godlewska1@wp.pl>
2020-02-11 23:21:10 +0100

    Day 12: one more exercise

--- src/js/day12/day12.js
+++ src/js/day12/day12.js
@@ -1,4 +1,6 @@
 const text = 'He earns 4000 euro from salary per month, 10000 euro annual bonus, 5500 euro online courses per month.'
 const pattern = /\d+/g
-const matches = text. match(pattern)
+const matches = text.match(pattern)
 console.log(matches)
+const sum=parseInt(matches[0]*12) + parseInt(matches[1]) + parseInt(matches[2]*12)
+console.log(sum) 
