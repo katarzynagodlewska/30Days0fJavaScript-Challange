fef3fe3ddedde9cf8873ec8f0997a27c7f071c6d
Kasia <katarzyna-godlewska1@wp.pl>
2020-02-12 23:48:06 +0100

    Day 14

---
+++ src/js/day14/day14.js
@@ -0,0 +1,11 @@
+try {
+    let numOne = 2
+    let sum = numOne + numTwo
+  } catch (err) {
+    console.error(err) 
+  } finally {
+    let numThree = 5
+    let squareNumThree = numThree * numThree
+    console.log(squareNumThree);
+  }
+
